79cc14afbcf02f41992524ec7d4fc7ccbda3da3e
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-07-04 14:52:25 +0100

    Make a list of secrets instead of manual prints.

--- main.py
+++ main.py
@@ -6,15 +6,21 @@
 
 ***REMOVED***
 
+***REMOVED***
+
     aws_secret = "REDACTED"
 
-    print(f"Accessed secret: {aws_secret}")
-
-***REMOVED***
 ***REMOVED***
 
 ***REMOVED***
-***REMOVED***print(f"Secret from environment: {secret}")
 ***REMOVED***
-***REMOVED***print("No secret found in environment variables.")
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
 ***REMOVED***

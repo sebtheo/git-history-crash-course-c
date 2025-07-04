3696a925dae75a2db4c7ce726941e9f8e30378bc
Kieran Pritchard <kieran.pritchard@ons.gov.uk>
2025-07-04 14:50:23 +0100

    Add .env support

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 ***REMOVED***
 ***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
     aws_secret = "REDACTED"
 
     print(f"Accessed secret: {aws_secret}")
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***print(f"Secret from environment: {secret}")
+***REMOVED***
+***REMOVED***print("No secret found in environment variables.")
 ***REMOVED***
